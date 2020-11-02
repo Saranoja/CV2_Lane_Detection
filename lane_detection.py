ce67dcf2fcb12e163a0d32da49126672149fed04
Saranoja <Saranoja@users.noreply.github.com>
2020-11-02 19:11:20 +0200

    feat: added initial processed version of the video: resized and grayscale

--- lane_detection.py
+++ lane_detection.py
@@ -1,19 +1,54 @@
 import cv2
 import numpy as np
 
+# comments only for check up
+
 cam = cv2.VideoCapture('Lane Detection Test Video 01.mp4')
+ret, frame = cam.read()
+
+
+def get_trapezoid_corners(window_width, window_height):
+    upper_left = (0.45 * window_width, 0.75 * window_height)
+    upper_right = (0.55 * window_width, 0.75 * window_height)
+    lower_left = (0, window_height)
+    lower_right = (window_width, window_height)
+    return upper_right, upper_left, lower_left, lower_right
+
+
+def get_resized_frame_dimensions(scale_ratio):
+    width = int(frame.shape[1] * scale_ratio)
+    height = int(frame.shape[0] * scale_ratio)
+    return width, height
+
+
+# dimensions = (width, height) tuple
+def resize_frame(dimensions):
+    resized_frame = cv2.resize(frame, dimensions)
+    return resized_frame
+
+
+def get_grayscale_frame(fr):
+    return cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
 
 
 def start_detection():
     while True:
+        global ret, frame
         ret, frame = cam.read()
+        
         if ret is False:
             break
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-        cv2.imshow('Original', frame)
+        # cv2.imshow('Original', frame)
+
+        width, height = get_resized_frame_dimensions(0.3)
+        resized_frame = resize_frame((width, height))
+        # cv2.imshow('Resized', resized_frame)
+
+        grayscale_frame = get_grayscale_frame(resized_frame)
+        cv2.imshow('Grayscale', grayscale_frame)
 
 
 if __name__ == "__main__":
