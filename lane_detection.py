64dea05fa0a9af9c505e1423775f2b865bfed309
mcrisan5537 <crisanmihai5537@gmail.com>
2020-11-06 16:03:33 +0200

    no

--- lane_detection.py
+++ lane_detection.py
@@ -63,7 +63,7 @@ def get_birds_eye_view(road_frame, width, height):
     return cv2.warpPerspective(road_frame, magic_matrix, (width, height))
 
 
-def get_burred_frame(fr):
+def get_blurred_frame(fr):
     return cv2.blur(fr, ksize=(3, 3))
 
 
@@ -98,6 +98,14 @@ def get_binary_frame(filtered_frame):
     return filtered_frame
 
 
+def remove_redundant_columns(width, fr):
+    fr_copy = fr.copy()
+    columns = int(width * 0.2)
+    fr_copy[0:, 0:columns] = 0
+    fr_copy[0:, -columns:] = 0
+    return fr_copy
+
+
 def get_white_points_coordinates(fr, width, height):
     left_slice = fr[0:height, 0:int(width / 2)]
     right_slice = fr[0:height, int(width / 2):]
@@ -112,17 +120,58 @@ def get_white_points_coordinates(fr, width, height):
     return left_xs, left_ys, right_xs, right_ys
 
 
-def remove_redundant_columns(width, fr):
-    fr_copy = fr.copy()
-    columns = int(width * 0.2)
-    fr_copy[0:, 0:columns] = 0
-    fr_copy[0:, -columns:] = 0
-    return fr_copy
+def get_line_points(fr, xs, ys, previous_top_x, previous_bottom_x):
+    if xs.size == 0 or ys.size == 0:
+        return tuple(map(lambda x: int(x), (previous_top_x, 0))), \
+               tuple(map(lambda x: int(x), (previous_bottom_x, fr.shape[0])))
+
+    a, b = np.polyfit(xs, ys, deg=1)
+
+    top_x = previous_top_x
+    top_y = 0
+
+    bottom_x = previous_bottom_x
+    bottom_y = fr.shape[0]
+
+    new_top_x = -b / a
+    new_bottom_x = (fr.shape[0] - b) / a
+    if -10 ** 8 <= new_top_x <= 10 ** 8 and -10 ** 8 <= new_bottom_x <= 10 ** 8:
+        top_x = new_top_x
+        bottom_x = new_bottom_x
+
+    return tuple(map(lambda x: int(x), (top_x, top_y))), tuple(map(lambda x: int(x), (bottom_x, bottom_y)))
+
+
+def add_lines(fr):
+    global previous_left_top_x, previous_left_bottom_x, previous_right_top_x, previous_right_bottom_x
+
+    left_xs, left_ys, right_xs, right_ys = get_white_points_coordinates(fr, fr.shape[1], fr.shape[0])
+    left_top_point, left_bottom_point = get_line_points(fr, left_xs, left_ys,
+                                                        previous_left_top_x, previous_left_bottom_x)
+    fr = cv2.line(fr, left_top_point, left_bottom_point, (100, 100, 100), 5)
+    previous_left_top_x, _ = left_top_point
+    previous_left_bottom_x, _ = left_bottom_point
+
+    right_top_point, right_bottom_point = get_line_points(fr, right_xs, right_ys,
+                                                          previous_right_top_x, previous_right_bottom_x)
+    previous_right_top_x, _ = right_top_point
+    previous_right_bottom_x, _ = right_bottom_point
+    fr = cv2.line(fr, right_top_point, right_bottom_point, (100, 100, 100), 5)
+
+    return fr
+
+
+# def get_good_visualization(fr):
+#     blank_frame = np.zeros((fr.shape[1], fr.shape[0]), dtype=np.uint8)
+#
+#
+#
+#     pass
 
 
 def start_detection():
     while True:
-        global ret, frame
+        global ret, frame, previous_left_top_x, previous_left_bottom_x, previous_right_top_x, previous_right_bottom_x
         ret, frame = cam.read()
 
         if ret is False:
@@ -148,18 +197,25 @@ def start_detection():
         top_down_view = get_birds_eye_view(grayscale_road_frame, width, height)
         # cv2.imshow('Birds eye', top_down_view)
 
-        blurred_top_down_view = get_burred_frame(top_down_view)
+        blurred_top_down_view = get_blurred_frame(top_down_view)
         # cv2.imshow('Blurred', blurred_top_down_view)
 
         sobel_filtered_top_down_view = combine_filtered_versions_of_frame(blurred_top_down_view)
         # cv2.imshow('Sobel-filtered', sobel_filtered_top_down_view)
 
         binary_filtered_top_down = get_binary_frame(sobel_filtered_top_down_view)
-        cv2.imshow('Binary', binary_filtered_top_down)
+        # cv2.imshow('Binary', binary_filtered_top_down)
 
-        reduced_binary = remove_redundant_columns(width, sobel_filtered_top_down_view)
+        reduced_binary = remove_redundant_columns(width, binary_filtered_top_down)
         # cv2.imshow('Binary and reduced', reduced_binary)
 
+        line_frame = add_lines(reduced_binary)
+        cv2.imshow('lines', line_frame)
+
+        blank_frame = np.zeros(width, height)
+
+
+previous_left_top_x, previous_left_bottom_x, previous_right_top_x, previous_right_bottom_x = 0, 0, 0, 0
 
 if __name__ == "__main__":
     start_detection()
